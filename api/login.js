a9e37bcfffa89e4c69cf8bb5c4a543bb9282630c
myhrtest1 <dummyfxo@gmail.com>
2025-09-06 18:27:17 +0530

    Update login.js

--- api/login.js
+++ api/login.js
@@ -39,7 +39,7 @@ export default async function handler(req, res) {
       return res.status(200).json({
         success: true,
         debug: {
-          matchedUser: records[0].fields.User,
+          matchedUser: records[0].fields.Usr,
           formula,
           recordsFound: records.length,
         }
